a74a8e5d435587bcf94e66cd7c80aad5b2c2cda9
Christopher R. Palmer <crpalmer@gmail.com>
2014-06-14 05:00:22 -0400

    mondrianwifi: wlan: Fixup the startup of wlan
    Steve Kondik just fixed the wcnss startup on m8.  These are a
    couple of changes that I was missing.

--- Android.mk
+++ Android.mk
@@ -28,6 +28,13 @@ ifeq ($(TARGET_BOARD_PLATFORM),msm8974)
 ifneq ($(filter mondrianlte,$(TARGET_DEVICE)),)
 
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+# Create a link for the WCNSS config file, which ends up as a writable
+# version in /data/misc/wifi
+$(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/prima; \
+    ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
+        $(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
+
 endif
 endif
 endif
--- device.mk
+++ device.mk
@@ -38,7 +38,7 @@ PRODUCT_PACKAGES += \
     libwcnss_qmi
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 

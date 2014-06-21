da97e67b84293481528489888bf5f07ba559d127
Christopher R. Palmer <crpalmer@gmail.com>
2014-06-21 06:55:45 -0400

    mondrianlte: Device specific fstab

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,5 +1,4 @@
 LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.bt.sh
@@ -7,5 +6,13 @@ LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+
 include $(BUILD_PREBUILT)
 

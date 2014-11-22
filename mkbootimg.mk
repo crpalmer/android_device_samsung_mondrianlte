9662b1e737a6709d2723506a6c557fdf2f7fc946
Christopher R. Palmer <crpalmer@gmail.com>
2014-11-22 06:06:53 -0500

    mondrianlte: Kang mkbootimg.sh from oppo-msm8974-common
    Conflicts:
    	mkbootimg.mk
    
    Change-Id: If687e0927cbf507bdd935f4c7a711e385328534b
    
    Conflicts:
    	mkbootimg.mk

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,29 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
-## Don't change anything under here. The variables are named MONDRIANLTE_whatever
-## on purpose, to avoid conflicts with similarly named variables at other
-## parts of the build environment
-
-## Imported from the original makefile...
-KERNEL_CONFIG := $(KERNEL_OUT)/.config
-MONDRIANLTE_DTS_NAMES := msm8974
-
-MONDRIANLTE_DTS_FILES = \
-   $(wildcard $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/arm/boot/dts/msm8974/apq8074-sec-mondrianwifi-*.dts) \
-   $(wildcard $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/arm/boot/dts/msm8974/msm8974-sec-mondrianlte-*.dts)
-
-MONDRIANLTE_DTS_FILE = $(lastword $(subst /, ,$(1)))
-DTB_FILE = $(addprefix $(KERNEL_OUT)/arch/arm/boot/,$(patsubst %.dts,%.dtb,$(call MONDRIANLTE_DTS_FILE,$(1))))
-ZIMG_FILE = $(addprefix $(KERNEL_OUT)/arch/arm/boot/,$(patsubst %.dts,%-zImage,$(call MONDRIANLTE_DTS_FILE,$(1))))
+CM_DTB_FILES = $(wildcard $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/arm/boot/*.dtb)
+CM_DTS_FILE = $(lastword $(subst /, ,$(1)))
+DTB_FILE := $(addprefix $(KERNEL_OUT)/arch/arm/boot/,$(patsubst %.dts,%.dtb,$(call CM_DTS_FILE,$(1))))
+ZIMG_FILE = $(addprefix $(KERNEL_OUT)/arch/arm/boot/,$(patsubst %.dts,%-zImage,$(call CM_DTS_FILE,$(1))))
 KERNEL_ZIMG = $(KERNEL_OUT)/arch/arm/boot/zImage
-DTC = $(KERNEL_OUT)/scripts/dtc/dtc
 
-define append-mondrianlte-dtb
+define append-cm-dtb
 mkdir -p $(KERNEL_OUT)/arch/arm/boot;\
-$(foreach MONDRIANLTE_DTS_NAME, $(MONDRIANLTE_DTS_NAMES), \
-   $(foreach d, $(MONDRIANLTE_DTS_FILES), \
-      $(DTC) -p 1024 -O dtb -o $(call DTB_FILE,$(d)) $(d); \
-      cat $(KERNEL_ZIMG) $(call DTB_FILE,$(d)) > $(call ZIMG_FILE,$(d));))
+$(foreach d, $(CM_DTB_FILES), \
+    cat $(KERNEL_ZIMG) $(call DTB_FILE,$(d)) > $(call ZIMG_FILE,$(d));)
 endef
 
 
@@ -33,9 +19,9 @@ INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
 
 $(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
 	@echo -e ${CL_CYN}"Start DT image: $@"${CL_RST}
-	$(call append-mondrianlte-dtb)
+	$(call append-cm-dtb)
 	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
-	$(DTBTOOL) -2 -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/arm/boot/
+	$(hide) $(DTBTOOL) -2 -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/arm/boot/
 	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
 
 
